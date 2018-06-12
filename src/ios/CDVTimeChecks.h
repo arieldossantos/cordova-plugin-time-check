7b1feeea49344ca1d84bf41ef9943ba119617a1b
Ariel Reis <reis.ariel@outlook.com>
2018-06-12 09:30:46 -0300

    Add iOS
    Adição das classes do iOS

---
+++ src/ios/CDVTimeChecks.h
@@ -0,0 +1,7 @@
+#import <Cordova/CDV.h>
+
+@interface CDVTimeChecks : CDVPlugin
+
+- (void) isAutomaticTimeZone:(CDVInvokedUrlCommand*)command;
+
+@end
---
+++ src/ios/CDVTimeChecks.m
@@ -0,0 +1,20 @@
+#import "CDVTimeChecks.h"
+#import <CoreLocation/CoreLocation.h>
+
+@implementation CDVTimeChecks
+
+- (void)isAutomaticTimeZone:(CDVInvokedUrlCommand*)command
+{
+    [self.commandDelegate runInBackground:^{
+        CDVPluginResult* pluginResult = nil;
+        if([NSTimeZone systemTimeZone] == [NSTimeZone defaultTimeZone]){
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        }else{
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Timezone is not automatic"];
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        }
+    }];
+}
+
+@end
